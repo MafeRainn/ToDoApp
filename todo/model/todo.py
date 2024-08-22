901e48628371e09d0b6054b566d95314ed37ffe3
MafeA <lorenarojasarango2@gmail.com>
2024-08-22 12:59:28 -0500

    arreglar problemas + def pending

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -14,17 +14,18 @@ class Todo:
             self.tags.append(tag)
     
     def __str__(self) -> str:
-        return f{self.code_id} - {self.title}
+        return {self.code_id} - {self.title}
     
 class TodoBook:
-    def __init__(self, todos:dict) -> None
-    self.todos: dict(int,Todo) = {}
+    def __init__(self, todos:dict) -> None:
+        self.todos: dict(int,Todo) = {}
     
-    def add_todo(self,title:str,description:str) ->int
-    code_id = len(self.todos) + 1
-    code_id= Todo(code_id:int, title:str, description:str)
-    todos = {code_id:Todo}
-    return code_id
+    def add_todo(self,title:str,description:str) ->int:
+        code_id = len(self.todos) + 1
+        code_id= Todo(code_id:int, title:str, description:str)
+        todos = {code_id:Todo}
+        return code_id
 
-
+    def pending_todos(self) -> list:Todo
+    return [todo for todo in self.todos.values() if not todo.completed]
         
